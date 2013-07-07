77799f088210f7d50dcd68951c2a307f20222993
Nikhil S <nklsrh@gmail.com>
2013-07-07 14:20:31 +1000

    WELCOME TO PONG, in 3D, with shadows, and cool cameras

--- Scripts/game.js
+++ Scripts/game.js
@@ -1,4 +1,16 @@
-var renderer, scene, camera;
+var renderer, scene, camera, pointLight, spotLight;
+
+var fieldWidth = 400, fieldHeight = 200;
+var paddleWidth, paddleHeight, paddleDepth, paddleQuality;
+
+var ball, paddle1, paddle2;
+var ballDirX = 1, ballDirY = 1, ballSpeed = 2;
+var paddle1DirY = 0, paddle2DirY = 0, paddleSpeed = 3;
+
+var score1 = 0, score2 = 0;
+
+// set opponent reflexes (0 - easiest, 1 - hardest)
+var difficulty = 0.5;
 
 function setup()
 {
@@ -31,18 +43,43 @@ function setup()
 
 	// // the camera starts at 0,0,0
 	// // so pull it back
-	camera.position.z = 300;
-
+	camera.position.z = 320;
+	
 	// // start the renderer
 	renderer.setSize(WIDTH, HEIGHT);
 
 	// // attach the render-supplied DOM element
 	c.appendChild(renderer.domElement);
 
+	// // set up the plane vars
+	var planeWidth = fieldWidth,
+		planeHeight = fieldHeight,
+		planeQuality = 10;
+		
+	// // create the sphere's material
+	var planeMaterial =
+	  new THREE.MeshLambertMaterial(
+		{
+		  color: 0x420000
+		});
+		
+	var plane = new THREE.Mesh(
+
+	  new THREE.PlaneGeometry(
+		planeWidth,
+		planeHeight,
+		planeQuality,
+		planeQuality),
+
+	  planeMaterial);
+	  
+	scene.add(plane);
+	plane.receiveShadow = true;	
+		
 	// // set up the sphere vars
-	var radius = 50,
-		segments = 36,
-		rings = 36;
+	var radius = 5,
+		segments = 26,
+		rings = 26;
 		
 	// // create the sphere's material
 	var sphereMaterial =
@@ -54,7 +91,7 @@ function setup()
 	// // create a new mesh with
 	// // sphere geometry - we will cover
 	// // the sphereMaterial next!
-	var sphere = new THREE.Mesh(
+	ball = new THREE.Mesh(
 
 	  new THREE.SphereGeometry(
 		radius,
@@ -64,26 +101,296 @@ function setup()
 	  sphereMaterial);
 
 	// // add the sphere to the scene
-	scene.add(sphere);
+	scene.add(ball);
+	ball.position.x = 0;
+	ball.position.y = 0;
+	ball.position.z = radius/2;
+	ball.receiveShadow = true;
+    ball.castShadow = true;
+	
+	// // set up the paddle vars
+	paddleWidth = 10;
+	paddleHeight = 30;
+	paddleDepth = 10;
+	paddleQuality = 10;
+		
+	paddle1 = new THREE.Mesh(
 
+	  new THREE.CubeGeometry(
+		paddleWidth,
+		paddleHeight,
+		paddleDepth,
+		paddleQuality,
+		paddleQuality,
+		paddleQuality),
+
+	  sphereMaterial);
+
+	// // add the sphere to the scene
+	scene.add(paddle1);
+	paddle1.receiveShadow = true;
+    paddle1.castShadow = true;
+	
+	paddle2 = new THREE.Mesh(
+
+	  new THREE.CubeGeometry(
+		paddleWidth,
+		paddleHeight,
+		paddleDepth,
+		paddleQuality,
+		paddleQuality,
+		paddleQuality),
+
+	  sphereMaterial);
+	  
+	// // add the sphere to the scene
+	scene.add(paddle2);
+	paddle2.receiveShadow = true;
+    paddle2.castShadow = true;	
+	
+	paddle1.position.x = -fieldWidth/2 + paddleWidth;
+	paddle2.position.x = fieldWidth/2 - paddleWidth;
+	
+	paddle1.position.z = paddleDepth;
+	paddle2.position.z = paddleDepth;
+	
 	// // create a point light
-	var pointLight =
+	pointLight =
 	  new THREE.PointLight(0xFFFFFF);
 
 	// set its position
-	pointLight.position.x = 10;
-	pointLight.position.y = 50;
-	pointLight.position.z = 130;
-
+	pointLight.position.x = -1000;
+	pointLight.position.y = 0;
+	pointLight.position.z = 1000;
+	pointLight.intensity = 2;
+	pointLight.distance = 10000;
 	// add to the scene
 	scene.add(pointLight);
+		
+    spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(0, 0, 460);
+    spotLight.intensity = 3;
+    spotLight.castShadow = true;
+    scene.add(spotLight);
+	
+	renderer.shadowMapEnabled = true;
+	
+	for (var i = 0; i < 10; i++)
+	{
+		var backdrop = new THREE.Mesh(
+
+		  new THREE.TorusKnotGeometry( 
+		  3200, 
+		  925, 
+		  32, 
+		  62, 
+		  32 ),
+
+		  sphereMaterial);
+		backdrop.rotation.z = i * (360 / 10) * Math.PI/180;
+		scene.add(backdrop);	
+	}
+	
+	score1 = 0;
+	score2 = 0;
 	
 	draw();
 }
 
+var time = 0;
+
 function draw()
 {	
+	time++;	
 	renderer.render(scene, camera);
-
 	requestAnimationFrame(draw);
+	
+	ballPhysics();
+	paddlePhysics();
+	playerPaddleMovement();
+	opponentPaddleMovement();
+	
+	cameraPhysics();
+}
+
+function ballPhysics()
+{
+	// if ball goes off the left side
+	if (ball.position.x <= -fieldWidth/2)
+	{	
+		score2++;
+		document.getElementById("scores").innerHTML = score1 + "-" + score2;
+		resetBall(2);
+	}
+	
+	// if ball goes off the right side
+	if (ball.position.x >= fieldWidth/2)
+	{	
+		score1++;
+		document.getElementById("scores").innerHTML = score1 + "-" + score2;
+		resetBall(1);
+	}
+	
+	// if ball goes off the top side
+	if (ball.position.y <= -fieldHeight/2)
+	{
+		ballDirY = -ballDirY;
+	}
+	
+	// if ball goes off the bottom side
+	if (ball.position.y >= fieldHeight/2)
+	{
+		ballDirY = -ballDirY;
+	}
+	
+	ball.position.x += ballDirX * ballSpeed;
+	ball.position.y += ballDirY * ballSpeed;
+	
+	if (ballDirY > ballSpeed * 2)
+	{
+		ballDirY = ballSpeed * 2;
+	}
+	else if (ballDirY < -ballSpeed * 2)
+	{
+		ballDirY = -ballSpeed * 2;
+	}
+}
+
+function opponentPaddleMovement()
+{
+	// Lerp towards the ball on the y plane
+	paddle2DirY = (ball.position.y - paddle2.position.y) * difficulty;
+	
+	// in case the Lerp function produces a value above max paddle speed, we clamp it
+	if (Math.abs(paddle2DirY) <= paddleSpeed)
+	{	
+		paddle2.position.y += paddle2DirY;
+	}
+	else
+	{
+		if (paddle2DirY > paddleSpeed)
+		{
+			paddle2.position.y += paddleSpeed;
+		}
+		else if (paddle2DirY < -paddleSpeed)
+		{
+			paddle2.position.y -= paddleSpeed;
+		}
+	}
+}
+
+function playerPaddleMovement()
+{
+	// move left
+	if (Key.isDown(Key.A))		
+	{
+		if (paddle1.position.y < fieldHeight * 0.45)
+		{
+			paddle1DirY = paddleSpeed * 0.5;
+		}
+		else
+		{
+			paddle1DirY = 0;
+			paddle1.scale.z += (10 - paddle1.scale.z) * 0.2;
+		}
+	}	
+	// move right
+	else if (Key.isDown(Key.D))
+	{
+		if (paddle1.position.y > -fieldHeight * 0.45)
+		{
+			paddle1DirY = -paddleSpeed * 0.5;
+		}
+		else
+		{
+			paddle1DirY = 0;
+			paddle1.scale.z += (10 - paddle1.scale.z) * 0.2;
+		}
+	}
+	// else don't move paddle
+	else
+	{
+		paddle1DirY = 0;
+	}
+			paddle1.scale.z += (1 - paddle1.scale.z) * 0.2;
+	
+	paddle1.position.y += paddle1DirY;
+}
+
+function cameraPhysics()
+{
+	// spotLight.position.x = ball.position.x;
+	// spotLight.position.y = ball.position.y;
+	camera.lookAt(new THREE.Vector3(paddle1.position.x - ball.position.x * 0.1, paddle1.position.y - ball.position.y * 0.1, ball.position.z));
+	
+	// move to behind the player's paddle
+	camera.position.x = paddle1.position.x - 100;
+	camera.position.y = paddle1.position.y;
+	camera.position.z = paddle1.position.z + 100 + 0.04 * (-ball.position.x + paddle1.position.x);
+	
+	// rotate to face towards the opponent
+	camera.rotation.y = -60 * Math.PI/180;
+	camera.rotation.z = -90 * Math.PI/180;
+}
+
+function paddlePhysics()
+{
+	// PLAYER PADDLE LOGIC
+	
+	// if ball is aligned with paddle1 on x plane
+	if (ball.position.x <= paddle1.position.x + paddleWidth
+	&&  ball.position.x >= paddle1.position.x)
+	{
+		// and if ball is aligned with paddle1 on y plane
+		if (ball.position.y <= paddle1.position.y + paddleHeight/2
+		&&  ball.position.y >= paddle1.position.y - paddleHeight/2)
+		{
+			// and if ball is travelling towards player (-ve direction)
+			if (ballDirX < 0)
+			{
+				ballDirX = -ballDirX;
+				if (paddle1DirY > 0.5)
+				{
+					ballDirY -= paddle1DirY * 0.7;
+				}
+			}
+		}
+	}
+	
+	// OPPONENT PADDLE LOGIC	
+	
+	// if ball is aligned with paddle2 on x plane
+	if (ball.position.x <= paddle2.position.x + paddleWidth
+	&&  ball.position.x >= paddle2.position.x)
+	{
+		// and if ball is aligned with paddle2 on y plane
+		if (ball.position.y <= paddle2.position.y + paddleHeight/2
+		&&  ball.position.y >= paddle2.position.y - paddleHeight/2)
+		{
+			// and if ball is travelling towards opponent (+ve direction)
+			if (ballDirX > 0)
+			{
+				ballDirX = -ballDirX;
+				if (paddle2DirY > 0.5)
+				{
+					ballDirY -= paddle2DirY * 0.7;
+				}
+			}
+		}
+	}
+}
+
+function resetBall(loser)
+{
+	ball.position.x = 0;
+	ball.position.y = 0;
+	if (loser == 1)
+	{
+		ballDirX = -1;
+	}
+	else
+	{
+		ballDirX = 1;
+	}
+	ballDirY = 1;
 }
---
+++ Scripts/keyboard.js
@@ -0,0 +1,26 @@
+window.addEventListener('keyup', function(event) { Key.onKeyup(event); }, false);
+window.addEventListener('keydown', function(event) { Key.onKeydown(event); }, false);
+
+var Key = {
+  _pressed: {},
+
+  A: 65,
+  W: 87,
+  D: 68,
+  S: 83,
+  SPACE: 32,
+  
+  isDown: function(keyCode) {
+    return this._pressed[keyCode];
+  },
+  
+  onKeydown: function(event) {
+    this._pressed[event.keyCode] = true;
+  },
+  
+  onKeyup: function(event) {
+    delete this._pressed[event.keyCode];
+  }
+};
+
+
